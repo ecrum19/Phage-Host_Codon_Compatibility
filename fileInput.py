490ecf89e7323133301bec3143e13585abca01a5
Isabella Bucciferro <50893921+ibucciferro@users.noreply.github.com>
2021-03-30 15:58:44 -0500

    Update fileInput.py

--- fileInput.py
+++ fileInput.py
@@ -1,8 +1,29 @@
 import os
+from Bio import Entrez, SeqIO
+Entrez.email = "ecrum@luc.edu"
+
+curr_path = os.getcwd()
 
 
-def parse(file_path):
+def getFasta(accession):
+    # Creates a fasta CDS file from a provided genbank accession
+
+    handle = Entrez.efetch(db="nucleotide", id=accession, rettype="gb", retmode="text")
+    record = SeqIO.read(handle, "genbank")
+    handle.close()
+
+    inSeq = curr_path + '/' + accession + '.fasta'
+    getSeq = open(inSeq, 'w')
+    for rec in record.features:     # iterate through list of features
+        if rec.type == "CDS":
+            getSeq.write(">" + str(record.id) + "\n")               # gene IDs
+            getSeq.write(str(rec.extract(record.seq)) + "\n")       # gene sequence
+    getSeq.close()
+
+
+def parseFasta(file_path):
     # Opens fasta CDS files, parses them and returns a dictionary containing the gene name and sequence
+
     f = open(file_path, 'r')
     fs = f.read().strip().split('>')
     final_genes = {}
@@ -13,11 +34,6 @@ def parse(file_path):
                 final_genes[si[0][4:]] = comp[12:].replace('\n', '')
     return final_genes
 
-# Test
-i = 'Phage.txt'
-j = 'Bacteria.txt'
-parse(i)
-parse(j)
 
 #create a definition to get the Key value for the dictionary
 def GetKey(val):
@@ -27,9 +43,12 @@ def GetKey(val):
       return "key doesn't exist"
 
 #create a definition to take the parsed file from the Phage,
-#take each gene and 
-def phageDict(phageGeneList):
+#and pull the coding sequence from the gene before breaking it into a list
+#of codons
+def phageCDS(phageGeneList):
     for gene in phageGeneList:
-        
-
-
+        codingseq = GetKey(gene)
+        #create a list of the codons present in this gene
+        codonlist = [codingseq[i:i+3] for i in range(0, len(codingseq), 3)]
+        #loop through the codon list 
+        for j in codonlist:
