77d8d23fd73dc4bd47a0d6d0905144eeddfbe929
Feras Altunusi <faltunusi1@luc.edu>
2021-04-22 15:55:23 -0500

    Update figureOutput.R

--- figureOutput.R
+++ figureOutput.R
@@ -11,4 +11,4 @@ library(ggplot2)
 dev <- ggplot(datatable, aes(y = Corr, x = "")) + geom_boxplot() + coord_flip() + ylab("Codon Correlation") + xlab('')
 
 #save the file as a PNG
-dev.print(file="corrPlot.png", device=png, width=800)
+dev.print(file = "corrPlot.png", device = png, width = 800)
